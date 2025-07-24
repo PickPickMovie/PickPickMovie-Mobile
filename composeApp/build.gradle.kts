28933d4fb2ee9e77744b1ed46f36384bbe475bb0
DoTheBestMayB <send1684@gmail.com>
2025-07-24 09:43:51 +0900

    build: 중복 sourceSet 설정 삭제

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -173,7 +173,6 @@ android {
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     buildTypes {
         release {
